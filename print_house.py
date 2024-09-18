88507458650110672988b7c9406b31eda3ac9e3e
Anton Menchaca <martinez.kar@northeastern.edu>
2024-09-17 21:40:02 -0400

    Update print_house.py

--- print_house.py
+++ print_house.py
@@ -22,8 +22,8 @@ def main():
     print('  + +  ')
     print(' +   + ')
     print('+-----+')
-
-
-
+    print('| .-. |')
+    print('| | | |')
+    print('+-+-+-+')
 if __name__ == '__main__':
     main()
